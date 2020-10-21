ecc6504ec15b20e3c2b4e83559c432ad12c4f23e
shaikmohammed9009 <shaikmohammed9009@gmail.com>
2020-10-22 01:08:31 +0530

    Updates

---
+++ precache-manifest.a4c5098d6f5521b02ca502a41035be86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03470dccb15679b24a24e59d88ac6f86",
+    "url": "/product/index.html"
+  },
+  {
+    "revision": "c200c46c3880dfea6ac6",
+    "url": "/product/static/css/main.5b2adf61.chunk.css"
+  },
+  {
+    "revision": "51f586d5278da86b5b4d",
+    "url": "/product/static/js/2.2f39c28f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/product/static/js/2.2f39c28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c200c46c3880dfea6ac6",
+    "url": "/product/static/js/main.5ad87f40.chunk.js"
+  },
+  {
+    "revision": "f752ad03cac220aa207e",
+    "url": "/product/static/js/runtime-main.1923b746.js"
+  }
+]);
